b2bad6c77e2c724faf490c992388f17fed884a17
Dario Dessaunet <me@dariodessaunet.dev>
2023-10-23 19:48:07 -0300

    Disable eslint on line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ function Home() {
                 </div>
                 <div className="l-datos t-datos">
                     <div className="l-datos-a">
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
                         <h3>Ayacucho 457, 1er piso, Oficina "12"</h3>
                         <h3>Telefax: 4951-6487</h3>
                     </div>
@@ -81,6 +82,7 @@ function Home() {
                     <footer className="l-footer-content t-footer-content">
                         <h1>Escribana M. Florencia LEPEZ MATINATI</h1>
                         <h3>Titular del Registro Notarial 267</h3>
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
                         <h3>Ayacucho 457, 1er piso, oficina "12", Ciudad Autonoma de Buenos Aires</h3>
                         <h3>Cel 15-63540707 - Telefono: 4951-6487</h3>
                     </footer>
