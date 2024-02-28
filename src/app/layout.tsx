e61ce36fad8537d65c97c264e7a646951fbab7d5
Dario Dessaunet <me@dariodessaunet.dev>
2024-02-28 00:26:47 -0300

    include scss files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
-// import './globals.css'
-import '../styles/base.css'
-import '../styles/layout.css'
-import '../styles/module.css'
-import '../styles/state.css'
-import '../styles/theme.css'
+import '../styles/style.scss'
+
 import AsideInformation from "@/core/components/AsideInformation";
 import Header from "@/core/components/Header";
 import Footer from "@/core/components/Footer";
